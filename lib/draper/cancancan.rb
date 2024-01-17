07f2c1f863b5c6faa34b82650de326c2579d7082
Norly Canarias <norly.canarias@cheetahdigital.com>
2024-01-17 10:43:43 +0800

    Align change with cancancan 3.0.0
    - Cancancan supports attribute-level rules added argument to can? so it
      also needs to be applied to Draper Cancancan extension to work
      correctly
    
    Refs
    - https://github.com/CanCanCommunity/cancancan/blob/main/CHANGELOG.md#300
    - https://github.com/CanCanCommunity/cancancan/pull/474

--- lib/draper/cancancan.rb
+++ lib/draper/cancancan.rb
@@ -3,12 +3,12 @@ require "draper/cancancan/version"
 module Draper
   module CanCanCan
     # actually don't need any code in my gem's namespace ..
-    def can?(action, subject, *extra_args)
+    def can?(action, subject, attribute=nil, *extra_args)
       while subject.is_a?(Draper::Decorator)
         subject = subject.model
       end
 
-      super(action, subject, *extra_args)
+      super(action, subject, attribute, *extra_args)
     end
 
     def cannot?(*args)
